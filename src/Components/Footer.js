a9b5095ed361d6bc4063bd85474b7e4336529592
JannatN <jannatfaisalnatsheh@gmail.com>
2020-07-24 15:56:42 +0300

    Version 5

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,38 +1,38 @@
 import React from 'react';
 import "./footer.css"
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 const Footer = (props) => {
-    return(
-        <div className ="footer">
+    return (
+        <div className="footer">
             <div className="day">{props.data.data0.day}
-            <p>{props.data.data0.temp}&deg;</p>
-            <img className="dayIcons" src={`http://openweathermap.org/img/w/${props.data.data0.icons}.png`}/><br></br>
-            <Link to="/details1" style={{ color: '#FFF' }}> DETAILS </Link>
+                <p>{props.data.data0.temp}&deg;</p>
+                <img className="dayIcons" src={`http://openweathermap.org/img/w/${props.data.data0.icons}.png`} /><br></br>
+                <Link to="/details1" style={{ color: '#FFF' }}> DETAILS </Link>
             </div>
-            
+
             <div className="day">{props.data.data1.day}
-            <p>{props.data.data1.temp}&deg;</p>
-            <img className="dayIcons" src={`http://openweathermap.org/img/w/${props.data.data1.icons}.png`}/><br></br>
-            <Link to="/details2" style={{ color: '#FFF' }}> DETAILS </Link>
+                <p>{props.data.data1.temp}&deg;</p>
+                <img className="dayIcons" src={`http://openweathermap.org/img/w/${props.data.data1.icons}.png`} /><br></br>
+                <Link to="/details2" style={{ color: '#FFF' }}> DETAILS </Link>
             </div>
             <div className="day">{props.data.data2.day}
-            <p>{props.data.data2.temp}&deg;</p>
-            <img className="dayIcons" src={`http://openweathermap.org/img/w/${props.data.data2.icons}.png`}/><br></br>
-            <Link to="/details3" style={{ color: '#FFF' }}> DETAILS </Link>
+                <p>{props.data.data2.temp}&deg;</p>
+                <img className="dayIcons" src={`http://openweathermap.org/img/w/${props.data.data2.icons}.png`} /><br></br>
+                <Link to="/details3" style={{ color: '#FFF' }}> DETAILS </Link>
             </div>
             <div className="day">{props.data.data3.day}
-            <p>{props.data.data3.temp}&deg;</p>
-            <img className="dayIcons" src={`http://openweathermap.org/img/w/${props.data.data3.icons}.png`}/><br></br>
-            <Link to="/details4" style={{ color: '#FFF' }}> DETAILS </Link>
+                <p>{props.data.data3.temp}&deg;</p>
+                <img className="dayIcons" src={`http://openweathermap.org/img/w/${props.data.data3.icons}.png`} /><br></br>
+                <Link to="/details4" style={{ color: '#FFF' }}> DETAILS </Link>
             </div>
             <div className="day">{props.data.data4.day}
-            <p>{props.data.data4.temp}&deg;</p>
-            <img className="dayIcons" src={`http://openweathermap.org/img/w/${props.data.data4.icons}.png`}/><br></br>
-            <Link to="/details5" style={{ color: '#FFF' }}> DETAILS </Link>
+                <p>{props.data.data4.temp}&deg;</p>
+                <img className="dayIcons" src={`http://openweathermap.org/img/w/${props.data.data4.icons}.png`} /><br></br>
+                <Link to="/details5" style={{ color: '#FFF' }}> DETAILS </Link>
             </div>
         </div>
     );
 }
 
-export default Footer ;
+export default Footer;
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,37 +1,32 @@
 import React from 'react'
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import "./Header.css"
 
 export default function Header(props) {
 
     return (
-        <header className="header">
-      <div className="container">
+        <div className="container">
             <div className="date">
-            <h5>Date|Time: {props.curTime}</h5>
+                <h5>Date|Time: {props.curTime}</h5>
             </div>
 
-            <div className="title">
-                <h1 >Today's Weather</h1>
-            </div>
-        
             <div className="search">
                 <form onSubmit={(e) => { props.changeLocation(e) }}>
-                    <input type="text" className="regionInput"  placeholder="Select region"
-                     onChange={(e) => { props.changeRegion(e.target.value) }} />
+                    <input type="text" className="regionInput" placeholder="Select region"
+                        onChange={(e) => { props.changeRegion(e.target.value) }} />
                 </form>
-            </div> 
+            </div>
 
             <div className="link1" >
-            <Link to="/" style={{ color: '#FFF' }}> HOME </Link>
+                <Link to="/" style={{ color: '#FFF' }}> HOME </Link>
             </div>
 
             <div className="link2" >
-            <Link to="/details" style={{ color: '#FFF' }}> DETAILS </Link>
+                <Link to="/details" style={{ color: '#FFF' }}> DETAILS </Link>
             </div>
 
-    </div>
-    </header>
+        </div>
+
 
     )
 }
